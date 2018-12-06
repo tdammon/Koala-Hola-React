a0d58961cfb5db3a417cc2a150aad8c399dd5f21
The Goose <trevordammon@gmail.com>
2018-12-06 12:05:10 -0600

    updated index file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import './index.css';
 import App from './App/App';
 import * as serviceWorker from './serviceWorker';
+import createSagaMiddleware from 'redux-saga';
+import logger from 'redux-logger'
+import koalaSaga from './redux/koalaSaga'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(logger)
+)
+
+sagaMiddleware.run(rootSaga)
+
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
